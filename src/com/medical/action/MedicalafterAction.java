45e487ed93d4afa3624aaaf7fe273f8ef5d2bc95
geogle2009 <geogle_jia@126.com>
2014-11-15 16:25:20 +0800

    2014-11-14

---
+++ src/com/medical/action/MedicalafterAction.java
@@ -0,0 +1,264 @@
+package com.medical.action;
+
+import java.util.List;
+import java.util.Map;
+
+import com.medical.common.Base64Image;
+import com.medical.common.FileUpload;
+import com.medical.dto.BaseInfoDTO;
+import com.medical.dto.MedicalafterDTO;
+import com.medical.dto.OrganDTO;
+import com.medical.dto.UserInfoDTO;
+import com.medical.model.JzMedicalafterExample;
+import com.medical.service.BaseinfoService;
+import com.medical.service.BusinessService;
+import com.medical.service.SearchService;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MedicalafterAction extends ActionSupport {
+	private static final long serialVersionUID = -5126976226423666862L;
+	private MedicalafterDTO medicalafterDTO;
+	private List<MedicalafterDTO> medicalafters;
+	private BaseinfoService baseinfoService;
+	private SearchService searchService;
+	private BaseInfoDTO baseInfoDTO;
+	private List<BaseInfoDTO> baseinfos;
+	private String result;
+	private BusinessService businessService;
+	private List<OrganDTO> orgs;
+	private String[] filebase64;
+
+	private String cur_page;
+	private String term;
+	private String oid;
+	private String operational;
+	private String toolsmenu;
+	private String value;
+
+	public String countassist() {
+		medicalafterDTO = baseinfoService.findCountAssist(medicalafterDTO);
+		return result;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public String afterquerymemberinit() {
+		Map session = ActionContext.getContext().getSession();
+		UserInfoDTO user = (UserInfoDTO) session.get("user");
+		if (user.getOrganizationId().length() == 4) {
+
+		} else {
+			result = "您所在的机构，没有审批权限！";
+			return "result";
+		}
+		return SUCCESS;
+	}
+
+	public String afterquerymember() {
+		// 查询人员基本信息
+		baseinfos = this.baseinfoService.findMemberByPaperId(baseInfoDTO);
+		medicalafters = this.baseinfoService
+				.findMedicalaftersByPaperId(baseInfoDTO);
+		if (baseinfos.size() == 0) {
+			result = "没有此人信息，请核实！";
+			return "result";
+		}
+		// 查询人员医后报销信息
+
+		return SUCCESS;
+	}
+
+	public String afterapplyinit() {
+		// 查询人员基本信息
+		medicalafterDTO = this.baseinfoService.findMemberByID(baseInfoDTO);
+		return SUCCESS;
+	}
+
+	public String afterapply() {
+		medicalafterDTO = this.baseinfoService.saveAfterApply(medicalafterDTO);
+		FileUpload fu = new FileUpload("/file/medicalafter");
+		String dir = fu.filepath + "\\" + medicalafterDTO.getMaId() + "\\";
+		fu.MakeDir(dir);
+		for (int i = 0; i < filebase64.length; i++) {
+			Base64Image.GenerateImage(filebase64[i], dir + (i + 1) + ".jpg");
+		}
+		return SUCCESS;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public String queryafterinit() {
+		Map session = ActionContext.getContext().getSession();
+		UserInfoDTO userinfo = (UserInfoDTO) session.get("user");
+		String orgid = userinfo.getOrganizationId();
+		if (4 == orgid.length()) {
+			this.setOrgs(this.businessService.getOrganList(userinfo
+					.getOrganizationId()));
+			return SUCCESS;
+		} else {
+			this.result = "您所在的机构，没有查询权限！";
+			return "result";
+		}
+
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public String queryafter() {
+		Map session = ActionContext.getContext().getSession();
+		UserInfoDTO userinfo = (UserInfoDTO) session.get("user");
+		String orgid = userinfo.getOrganizationId();
+		JzMedicalafterExample example = new JzMedicalafterExample();
+		if (null == cur_page || "".equals(cur_page)) {
+			com.medical.model.JzMedicalafterExample.Criteria criteria = example
+					.createCriteria();
+			criteria.andOnNoLike(oid + "%");
+			if ("SSN".equals(term)) {
+				criteria.andSsnLike(value + "%");
+			} else if ("FAMILYNO".equals(term)) {
+				criteria.andFamilynoLike(value + "%");
+			} else if ("MEMBERNAME".equals(term)) {
+				criteria.andMembernameLike(value + "%");
+			} else if ("PAPERID".equals(term)) {
+				criteria.andPaperidLike(value + "%");
+			} else {
+			}
+			session.put("sql", example);
+			cur_page = "1";
+		} else {
+			example = (JzMedicalafterExample) session.get("sql");
+		}
+
+		this.setMedicalafters(this.baseinfoService.queryMedicalafters(example,
+				new Integer(cur_page)));
+		this.setToolsmenu(this.businessService.getPager().getToolsmenu());
+		this.setOrgs(this.businessService.getOrganList(orgid));
+		return SUCCESS;
+	}
+
+	public BaseinfoService getBaseinfoService() {
+		return baseinfoService;
+	}
+
+	public void setBaseinfoService(BaseinfoService baseinfoService) {
+		this.baseinfoService = baseinfoService;
+	}
+
+	public SearchService getSearchService() {
+		return searchService;
+	}
+
+	public void setSearchService(SearchService searchService) {
+		this.searchService = searchService;
+	}
+
+	public List<MedicalafterDTO> getMedicalafters() {
+		return medicalafters;
+	}
+
+	public void setMedicalafters(List<MedicalafterDTO> medicalafters) {
+		this.medicalafters = medicalafters;
+	}
+
+	public MedicalafterDTO getMedicalafterDTO() {
+		return medicalafterDTO;
+	}
+
+	public void setMedicalafterDTO(MedicalafterDTO medicalafterDTO) {
+		this.medicalafterDTO = medicalafterDTO;
+	}
+
+	public BaseInfoDTO getBaseInfoDTO() {
+		return baseInfoDTO;
+	}
+
+	public void setBaseInfoDTO(BaseInfoDTO baseInfoDTO) {
+		this.baseInfoDTO = baseInfoDTO;
+	}
+
+	public List<BaseInfoDTO> getBaseinfos() {
+		return baseinfos;
+	}
+
+	public void setBaseinfos(List<BaseInfoDTO> baseinfos) {
+		this.baseinfos = baseinfos;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public BusinessService getBusinessService() {
+		return businessService;
+	}
+
+	public void setBusinessService(BusinessService businessService) {
+		this.businessService = businessService;
+	}
+
+	public List<OrganDTO> getOrgs() {
+		return orgs;
+	}
+
+	public void setOrgs(List<OrganDTO> orgs) {
+		this.orgs = orgs;
+	}
+
+	public String getCur_page() {
+		return cur_page;
+	}
+
+	public void setCur_page(String cur_page) {
+		this.cur_page = cur_page;
+	}
+
+	public String getTerm() {
+		return term;
+	}
+
+	public void setTerm(String term) {
+		this.term = term;
+	}
+
+	public String getToolsmenu() {
+		return toolsmenu;
+	}
+
+	public void setToolsmenu(String toolsmenu) {
+		this.toolsmenu = toolsmenu;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getOid() {
+		return oid;
+	}
+
+	public void setOid(String oid) {
+		this.oid = oid;
+	}
+
+	public String getOperational() {
+		return operational;
+	}
+
+	public void setOperational(String operational) {
+		this.operational = operational;
+	}
+
+	public String[] getFilebase64() {
+		return filebase64;
+	}
+
+	public void setFilebase64(String[] filebase64) {
+		this.filebase64 = filebase64;
+	}
+}
